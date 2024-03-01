f42def026072f7c4fdca6491a7ca8ae723855134
YashwanthKumar02 <yashwanthnani2@gmail.com>
2024-03-01 14:40:07 +0530

    roll back db9fa6c

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Counter</h1>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Vite</h1>
     </>
   )
 }
